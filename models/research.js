fbcbf1152eb1b26e82f176ea0ce2ee29f4cc40db
Brian Mugweru <brian@ralphowino.com>
2018-11-30 08:45:50 +0300

    change researhcdata to research

---
+++ models/research.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ResearchModel = new Schema({
+    title : {type: String, required: [true, 'More than anything, title is definitely required']},
+    mine: [{type: Schema.Types.ObjectId, ref: 'Mine' }],
+    findings: { type: String },
+    metadata: {type: String },
+}, { timestamps: { createdAt: 'created_at' }});
+
+export default mongoose.model('research', ResearchModel);
